8dbffd4eda8062fea4725023915eccb18ccc74fa
Jacob Banks <kobybanks@gmail.com>
2021-08-21 05:31:52 -0400

    added post modle and routes, first assosition

--- models/index.js
+++ models/index.js
@@ -1,9 +1,16 @@
 // import all models
-//const Post = require('./Post');
+const Post = require("./Post");
 const User = require("./User");
 //const Vote = require('./Vote');
 //const Comment = require('./Comment');
 
 // create associations
-module.exports = { User };
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+module.exports = { User, Post };
 // Post, Vote, Comment
