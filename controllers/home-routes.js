1aeebec0feac394e16487aa70280480a5a1a9f6a
Jacob Banks <kobybanks@gmail.com>
2021-08-21 11:24:54 -0400

    fixed login/out display rederict to dashboar d on sign in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -74,7 +74,7 @@ router.get("/post/:id", (req, res) => {
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect("/dashboard");
     return;
   }
 
