f5d86e5ce45f20a0df024b9342608f6ec2ac1dad
Jacob Banks <kobybanks@gmail.com>
2021-08-23 14:30:43 -0400

    finally figured out bug! when trying to fix postss not deleting changed a column name in post model thinking it might be reserved, but the database never updated thhe change.

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,8 +1,8 @@
 async function newFormHandler(event) {
   event.preventDefault();
-
+  alert("hi there");
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_text = document.querySelector('input[name="post-text"]').value;
+  const post_text = document.getElementById("post-text").value;
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
@@ -16,7 +16,7 @@ async function newFormHandler(event) {
   });
 
   if (response.ok) {
-    // alert(post_text + " " + title);
+    alert(post_text + " " + title);
     document.location.replace("/dashboard");
   } else {
     alert(response.statusText);
